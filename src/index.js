91b87eb75717b70b3d71426f19c0d2b9472bd4ef
정용채 <asd0905@naver.com>
2021-03-16 15:44:51 +0900

    feat: App 컴포넌트를 화면에 뿌려주기 (DOM)

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import React from 'react'; // 컴포넌트의 기능 (모으고 합체하고)
+import ReactDOM from 'react-dom'; // 모아온 컴포넌트를 dom에 랜더링할 때 필요, 그래서 랜더할때도 ReactDOM
 
-import App from './components/app';
-import reducers from './reducers';
+// Create a new component. this component should produce
+// some HTML
+// HTML을 제공하는 컴포넌트를 생성한다
+const App = () => {
+    return <div>HI!</div>
+}
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+// 생성한 HTML(컴포넌트가 return한)를 DOM에 넣어준다.
+// Take this component's generated HTML and put it
+// on the page (in the DOM)
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+ReactDOM.render(<App />, document.querySelector('.container'));
